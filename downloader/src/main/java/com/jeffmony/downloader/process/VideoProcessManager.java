dc0bfecb4011e089d55ffb4341a495dc6da5a25a
华仔 <2274680923@qq.com>
2022-08-31 18:49:35 +0800

    更换精简库

--- downloader/src/main/java/com/jeffmony/downloader/process/VideoProcessManager.java
+++ downloader/src/main/java/com/jeffmony/downloader/process/VideoProcessManager.java
@@ -2,8 +2,9 @@ package com.jeffmony.downloader.process;
 
 import androidx.annotation.NonNull;
 
-import com.jeffmony.downloader.utils.LogUtils;
-import com.jeffmony.ffmpeglib.FFmpegVideoUtils;
+//import com.jeffmony.downloader.utils.LogUtils;
+//import com.jeffmony.ffmpeglib.FFmpegVideoUtils;
+import com.jeffmony.m3u8library.listener.IVideoTransformListener;
 
 import java.io.File;
 
@@ -25,22 +26,44 @@ public class VideoProcessManager {
     }
 
     public void mergeTs(String inputPath, String outputPath, @NonNull IM3U8MergeListener listener) {
-        int result = FFmpegVideoUtils.transformVideo(inputPath, outputPath);
-        LogUtils.i(TAG, "VideoMerge mergeTs result=" +result);
-        if (result < 0) {
-            listener.onMergeFailed(new Exception("Merge ts failed"));
-        } else {
-            File outputFile = new File(outputPath);
-            if (outputFile.exists()) {
-                listener.onMergedFinished();
-            } else {
-                listener.onMergeFailed(new Exception("Merge ts failed, No file"));
+//        int result = FFmpegVideoUtils.transformVideo(inputPath, outputPath);
+//        LogUtils.i(TAG, "VideoMerge mergeTs result=" +result);
+//        if (result < 0) {
+//            listener.onMergeFailed(new Exception("Merge ts failed"));
+//        } else {
+//            File outputFile = new File(outputPath);
+//            if (outputFile.exists()) {
+//                listener.onMergedFinished();
+//            } else {
+//                listener.onMergeFailed(new Exception("Merge ts failed, No file"));
+//            }
+//        }
+
+        com.jeffmony.m3u8library.VideoProcessManager.getInstance().transformM3U8ToMp4(inputPath, outputPath, new IVideoTransformListener() {
+            @Override
+            public void onTransformProgress(float progress) {
+
             }
-        }
+
+            @Override
+            public void onTransformFailed(Exception e) {
+                listener.onMergeFailed(new Exception("Merge ts failed", e));
+            }
+
+            @Override
+            public void onTransformFinished() {
+                File outputFile = new File(outputPath);
+                if (outputFile.exists()) {
+                    listener.onMergedFinished();
+                } else {
+                    listener.onMergeFailed(new Exception("Merge ts failed, No file"));
+                }
+            }
+        });
     }
 
     public void printVideoInfo(String srcPath) {
-        FFmpegVideoUtils.printVideoInfo(srcPath);
+//        FFmpegVideoUtils.printVideoInfo(srcPath);
     }
 
 }
